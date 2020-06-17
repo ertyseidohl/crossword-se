fa31e8c431072043023cb6ecc0d241ecdc44ec92
Erty Seidohl <ertysdl+github@gmail.com>
2020-06-16 21:23:33 -0500

    Add test server, add test for word completions and focus

---
+++ TestServer.py
@@ -0,0 +1,42 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+from threading import Thread
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def _set_headers(self, status):
+        self.send_response(status)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+
+    def do_GET(self):
+        """Respond to GET requests"""
+        print('GET ' + self.path)
+        if self.path == '/words?word=...&page=0':
+            self._set_headers(200)
+            self.wfile.write(b'AAA BBB CCC DDD EEE FFF GGG HHH III JJJ')
+        elif self.path == '/words?word=H....&page=0':
+            self._set_headers(200)
+            self.wfile.write(b'LLL MMM NNN OOO PPP QQQ RRR SSS TTT UUU')
+        elif self.path == '/solve?crossword=...%0A...%0A...':
+            self._set_headers(200)
+            self.wfile.write(b'ERA\nREL\nEDE')
+        else:
+            self._set_headers(404)
+            self.wfile.write(b'404 Not Found')
+
+
+class TestServer:
+    def __init__(self):
+        self._running = True
+        self.httpd = HTTPServer(('localhost', 8079), SimpleHTTPRequestHandler)
+        self.thread = Thread(target=self.httpd.serve_forever)
+        self.thread.daemon = True
+
+    def run(self):
+        """Start the server thread."""
+        self.thread.start()
+
+    def terminate(self):
+        """Shutdown the server thread."""
+        self.httpd.shutdown()
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-"""Tests for Crossword-fe."""
 import unittest
 
 from selenium import webdriver
@@ -7,32 +6,127 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
+from TestServer import TestServer
+
+
 def clear_text(element):
+    """Remove all text from an element. `.clear` is broken in Chrome."""
     length = len(element.get_attribute('value'))
     element.send_keys(length * Keys.BACKSPACE)
 
 class CrosswordTests(unittest.TestCase):
     """Tests for crossword app"""
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+
     def setUp(self):
         super(CrosswordTests, self).setUp()
-        self.driver = webdriver.Chrome()
-        self.driver.get('http://localhost:8081/')
+        self.driver.get('http://localhost:8080/')
         WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.ID, "app")))
+            EC.presence_of_element_located((By.ID, 'app')))
+        self._set_up_test_server()
+        self._set_size_to_3x3()
 
-    def tearDown(self):
-        self.driver.close()
+    def _set_up_test_server(self):
+        self.driver.execute_script('window.replaceServer("//localhost:8079")')
 
-    def test_set_size(self):
-        """Test the width and height inputs work"""
+    def _set_size_to_3x3(self):
         width = self.driver.find_element_by_id('width')
         clear_text(width)
         width.send_keys('3')
         height = self.driver.find_element_by_id('height')
         clear_text(height)
         height.send_keys('3')
+
+    def _get_focused_element(self):
+        # Rename this so it's easier to understand.
+        return self.driver.switch_to.active_element
+
+    def test_set_size(self):
+        """Make sure that the grid resizes properly."""
+        # We call _set_size_to_3x3 in our setup.
+        # A 3x3 grid should have 9 cells
         cells = self.driver.find_elements_by_class_name('cell')
-        assert len(cells) == 9
+        self.assertEqual(len(cells), 9)
+
+        # Without any dark squares, we should have 3 across and 3 down clues
+        clues = self.driver.find_elements_by_class_name('clue')
+        self.assertEqual(len(clues), 6)
+
+
+    def test_get_word_completions(self):
+        """Test that we can get word completions from the test server"""
+        first_cell = self.driver.find_element_by_id('0,0')
+        first_cell.click()
+        first_cell.send_keys(Keys.RETURN)
+
+        # The exploreword should be set to the value of the current word.
+        # We have set the size to 3x3 in our setup.
+        exploreword = self.driver.find_element_by_id('exploreword')
+        self.assertEqual(exploreword.get_attribute('value'), '...')
+
+        # Wait for the words to load
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'word')))
+
+        # Make sure that our test words have loaded.
+        words = self.driver.find_elements_by_class_name('word')
+        self.assertEqual(len(words), 10)
+        self.assertEqual(words[0].get_attribute('value'), 'AAA')
+        self.assertEqual(words[9].get_attribute('value'), 'JJJ')
+
+    def test_tab_switching(self):
+        """Test that switching from element to element using tab works."""
+
+        # Hitting tab from the crossword should take us to the current clue.
+        # In a 3x3, the center cell will be part of 4 across.
+        center_cell = self.driver.find_element_by_id('1,1')
+        center_cell.click()
+        center_cell.send_keys(Keys.TAB)
+
+        # 0,1,1 = 4 across
+        center_cell_across_clue = self.driver.find_element_by_id('0,1,1')
+
+        self.assertEqual(self._get_focused_element(), center_cell_across_clue)
+
+        # Hitting tab from the clue should take us to the explore word
+        center_cell_across_clue.send_keys(Keys.TAB)
+        exploreword = self.driver.find_element_by_id('exploreword')
+        self.assertEqual(self._get_focused_element(), exploreword)
+
+        # Hitting tab from the explore word should take us to the wordlist.
+        exploreword.send_keys(Keys.TAB)
+        wordlist = self.driver.find_element_by_id('wordlist')
+        self.assertEqual(self._get_focused_element(), wordlist)
+
+        # Hitting tab from the wordlist should take us to the desired wordlist
+        wordlist.send_keys(Keys.TAB)
+        desired_words = self.driver.find_element_by_id('desiredwords')
+        self.assertEqual(self._get_focused_element(), desired_words)
+
+        # Hitting shift+tab from the desired wordlist should take us back to the wordlist.
+        desired_words.send_keys(Keys.SHIFT + Keys.TAB)
+        self.assertEqual(self._get_focused_element(), wordlist)
+
+        # Hitting shift+tab from the wordlist should take us back to the exploreword.
+        wordlist.send_keys(Keys.SHIFT + Keys.TAB)
+        self.assertEqual(self._get_focused_element(), exploreword)
+
+        # Hitting shift+tab from the exploreword should take us back to the clue.
+        exploreword.send_keys(Keys.SHIFT + Keys.TAB)
+        self.assertEqual(self._get_focused_element(), center_cell_across_clue)
+
+        # Hitting shift+tab from the clue should take us back to the center cell.
+        center_cell_across_clue.send_keys(Keys.SHIFT + Keys.TAB)
+        self.assertEqual(self._get_focused_element(), center_cell)
 
 if __name__ == '__main__':
+    test_server = TestServer()
+    test_server.run()
     unittest.main()
+    test_server.terminate()
